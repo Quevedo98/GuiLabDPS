f43a8794cd00977a7e75aad6d317a61a45402a31
luigichevez19 <luigichevez@gmail.com>
2020-08-22 14:33:25 -0600

    Ejercicio 3 y 4 100% s

--- Guia02/EjerciciosTypescript.js
+++ Guia02/EjerciciosTypescript.js
@@ -23,18 +23,20 @@ var Empleado = /** @class */ (function () {
         this.nombre = nombre;
         this.salario = salario;
     }
-    Empleado.prototype.deduccionessalariales = function (des) {
-        var total;
-        total = this.salario * des;
+    Empleado.prototype.deduccionessalariales = function () {
+        var total = this.salario * 0.10;
         return total;
     };
     return Empleado;
 }());
+var to = new Empleado("Luis", 155);
+var respu = to.deduccionessalariales();
+console.log("El sueldo es = " + respu);
 //Ejercicio 4
 var Calculadora = /** @class */ (function () {
-    function Calculadora(n1, n2) {
-        this.numero1 = n1;
-        this.numero2 = n2;
+    function Calculadora(numero1, numero2) {
+        this.numero1 = numero1;
+        this.numero2 = numero2;
     }
     Calculadora.prototype.sumar = function () {
         return this.numero1 + this.numero2;
@@ -49,10 +51,10 @@ var Calculadora = /** @class */ (function () {
         return this.numero1 * this.numero2;
     };
     Calculadora.prototype.calculo = function () {
-        console.log("Suma:" + this.sumar());
-        console.log("Resta:" + this.resta());
-        console.log("Division:" + this.div());
-        console.log("Multiplicacion:" + this.mul());
+        return "Suma:" + this.sumar() + "\n" + "Resta:" + this.resta() + "\n" + "Division:" + this.div() + "\n" + "Multiplicacion:" + this.mul() + "\n";
     };
     return Calculadora;
 }());
+var calculador = new Calculadora(10, 5);
+var respuestaf = calculador.calculo();
+console.log("Las operaciones de los numero " + calculador.numero1 + " y " + calculador.numero2 + "\n" + respuestaf);
